0e906c360efcd4dfda0309a2c791f0ec37cdd406
ScidaP <patricioscida@gmail.com>
2021-04-15 09:50:50 -0300

    Ej 2 cambiado a aritmetica de puntero

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -3,7 +3,17 @@
 #include <stdlib.h>
 
 int main () {
-    
+    int filas = 4;
+    int columnas = 4;
+    int * matriz = (int *) malloc(filas * columnas * sizeof(int));
+    int i, j;
+    for (i = 0; i < filas; i++) {
+        for (j = 0; j < columnas; j++) {
+            matriz[i*columnas+j] = i * columnas + j;
+            printf("%4d", matriz[i*columnas+j]);
+        }
+        printf("\n");
+    }
     getchar();
     return 0;
 }
